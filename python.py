194e62dae89aa9ae5a2c5e8cceff3b35eb09c6a7
bharathigubbala <bharathigubbala765@gmail.com>
2024-04-11 12:09:54 +0530

    python changes

---
+++ python.py
@@ -0,0 +1,10 @@
+def sum_of_two_numbers(num1, num2):
+    return num1 + num2
+
+# Example usage
+number1 = 5
+number2 = 3
+result = sum_of_two_numbers(number1, number2)
+
+
+print(f"The sum of {number1} and {number2} is {result}.")
